89bbd590ccc5a9bdf6c730f1df1a3bfb62fd8c36
Itay.R <itayrafee@gmail.com>
2022-07-03 15:28:06 +0300

    Update CNN

--- InitialOperations.py
+++ InitialOperations.py
@@ -11,21 +11,43 @@ labels_dec = {'unknown': 0, 'head_lamp': 1, 'door_scratch': 2, 'door_dent': 3, '
               'bumper_dent': 6, 'bumper_scratch': 7}
 
 
-def split_train_test(vectors_path):
+def get_csv_labels():
     labels = []
-    with open(vectors_path, 'rb') as f:
-        data = pickle.load(f)
     file = open('database/data.csv')
     csvreader = csv.reader(file)
     next(csvreader)
     for row in csvreader:
         labels.append(labels_dec[row[2]])
-    data = np.array(data)
-    labels = np.array(labels)
+    return np.array(labels)
+
+
+def split_train_test(vectors_path):
+    with open(vectors_path, 'rb') as f:
+        data = pickle.load(f)
+        data = np.array(data)
+    labels = get_csv_labels()
     X_train, X_test, y_train, y_test = train_test_split(
         data, labels, test_size=0.2)
-    # print(type(X_test))
+    return X_train, X_test, y_train, y_test
+
+
+def get_data(project_path):
+    data = []
+    n = 1512
+    for i in range(n):
+        img = Image.open(project_path+'/'+str(i)+'.jpg')
+        array = np.array(img)
+        data.append(array)
+
+    data = np.array(data)
+    data = data.reshape((data.shape[0], -1))
+    # Normalize
+    # data = data / 255
+
+    labels = get_csv_labels()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, labels, test_size=0.2)
     # print(X_test.shape)
-    #
     # exit()
     return X_train, X_test, y_train, y_test
--- algorithms/CNN.py
+++ algorithms/CNN.py
@@ -1,6 +1,7 @@
+import numpy as np
 from keras import Sequential
 from keras.callbacks import LambdaCallback
-from keras.layers import Flatten, Dense, Activation, MaxPooling2D, Conv2D, MaxPool2D
+from keras.layers import Flatten, Dense, Activation, MaxPooling2D, Conv2D, MaxPool2D, Conv1D, Dropout, MaxPooling1D
 from matplotlib import pyplot as plt
 
 
@@ -11,12 +12,12 @@ def evaluate_test(model, X_test, y_test, test_acc, test_loss):
 
 
 def CNN(X_train, X_test, y_train, y_test):
-    ROW = 100
-    COL = 10
+    ROW = 45
+    COL = 20
     CHANNEL = 1
 
-    X_train = X_train.reshape(X_train.shape[0], ROW, COL, CHANNEL)
-    X_test = X_test.reshape(X_test.shape[0], ROW, COL, CHANNEL)
+    # X_train = X_train.reshape(X_train.shape[0], ROW, COL, CHANNEL)
+    # X_test = X_test.reshape(X_test.shape[0], ROW, COL, CHANNEL)
     model = Sequential()
 
     model.add(Conv2D(256, (3, 3), input_shape=X_train.shape[1:]))
@@ -81,3 +82,30 @@ def CNN(X_train, X_test, y_train, y_test):
     plt.title('train and validation accuracy')
     plt.legend()
     plt.show()
+
+
+def CNN2(X_train, y_train, X_test, y_test):
+    print(X_train.shape)
+    print(y_train.shape)
+    verbose, epochs, batch_size = 0, 10, 32
+    print(X_train.shape)
+    n_outputs = y_train.shape[0]
+    y_train = y_train.reshape(1209, )
+    # n_timesteps, n_features, n_outputs = X_train.shape[0], X_train.shape[1], y_train.shape[0]
+    # X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    model = Sequential()
+    # model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
+    model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(n_outputs, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit network
+
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    # evaluate model
+    _, accuracy = model.evaluate(X_test, y_test, batch_size=batch_size, verbose=0)
+    print(accuracy*100)
+    # return accuracy
