3caeb49b5bc686621264ecec88be8e31697aa4e6
AlmogJakov <almog1006@gmail.com>
2022-07-03 18:11:28 +0300

    bilateral database removed

--- algorithms/CNN.py
+++ algorithms/CNN.py
@@ -1,8 +1,12 @@
+import numpy as np
 from keras import Sequential
 from keras.callbacks import LambdaCallback
-from keras.layers import Flatten, Dense, Activation, MaxPooling2D, Conv2D, MaxPool2D
+from keras.layers import Flatten, Dense, Activation, MaxPooling2D, Conv2D, MaxPool2D, Conv1D, Dropout, MaxPooling1D
 from matplotlib import pyplot as plt
+from tensorflow import keras
 
+
+# https://www.kaggle.com/code/anandhuh/image-classification-using-cnn-for-beginners/notebook
 
 def evaluate_test(model, X_test, y_test, test_acc, test_loss):
     res_test = model.evaluate(X_test, y_test)
@@ -10,74 +14,61 @@ def evaluate_test(model, X_test, y_test, test_acc, test_loss):
     test_acc.append(res_test[1])
 
 
-def CNN(X_train, X_test, y_train, y_test):
-    ROW = 100
-    COL = 10
-    CHANNEL = 1
+def cnn(X_train, X_test, y_train, y_test):
+    # ROW = 45
+    # COL = 45
+    # CHANNEL = 3
+    # X_train = X_train.reshape(X_train.shape[0], ROW, COL, CHANNEL)
+    # X_test = X_test.reshape(X_test.shape[0], ROW, COL, CHANNEL)
 
-    X_train = X_train.reshape(X_train.shape[0], ROW, COL, CHANNEL)
-    X_test = X_test.reshape(X_test.shape[0], ROW, COL, CHANNEL)
+    # model = Sequential()
+    # model.add(Conv2D(128, (3, 3), input_shape=X_train.shape[1:]))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Conv2D(64, (3, 3)))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model = Sequential()
+    model.add(Conv2D(45, (3, 3), activation='relu', input_shape=X_train.shape[1:]))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(62, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(62, (3, 3), activation='relu'))
 
-    model.add(Conv2D(256, (3, 3), input_shape=X_train.shape[1:]))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(128, (3, 3), input_shape=X_train.shape[1:]))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-
+    model.add(Flatten())  # converts 3D feature maps to 1D feature vectors
+    model.add(Dense(62, activation='relu'))
     model.add(Dense(32))
 
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
-
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
+    model.compile(optimizer='adam', loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
+    history = model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test))
+    plt.plot(history.history['accuracy'], label='train accuracy')
+    plt.plot(history.history['val_accuracy'], label='test accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.25, 1])
+    plt.legend(loc='lower right')
+    plt.show()
 
-    epochs = 6
-    test_error = []
-    test_acc = []
-    myCallback = LambdaCallback(on_epoch_end=lambda batch, logs: evaluate_test(model, X_test, y_test, test_acc, test_error))
-    details = model.fit(X_train, y_train, batch_size=128, epochs=epochs, validation_split=0.214, callbacks=[myCallback])
-
-    val_acc_list = details.history['val_accuracy']
-    acc_list = details.history['accuracy']
-    loss_list = details.history['loss']
-    val_loss_list = details.history['val_loss']
     results = model.evaluate(X_test, y_test, verbose=0)
-
     print("final accuracy: {:5.2f}%".format(100 * results[1]))
 
-    x_axis = []
-    k = 1
-    for i in range(1, epochs + 1):
-        x_axis.append(k)
-        k = k + 1
 
-    # Loss graph
-    plt.plot(x_axis, test_error, label="test loss")
-    plt.plot(x_axis, val_loss_list, label="validation loss")
-    plt.plot(x_axis, loss_list, label="train loss")
-    plt.xlabel('epochs')
-    plt.ylabel('value')
-    plt.title('train and validation loss')
-    plt.legend()
+def cnn_via_vgg(X_train, X_test, y_train, y_test):
+    model = Sequential()
+    # model.add(Flatten())  # converts 3D feature maps to 1D feature vectors
+    model.add(Dense(62, activation='relu'))
+    model.add(Dense(32))
+    model.compile(optimizer='adam', loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+    plt.plot(history.history['accuracy'], label='train accuracy')
+    plt.plot(history.history['val_accuracy'], label='test accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.25, 1])
+    plt.legend(loc='lower right')
     plt.show()
-
-    # Accuracy graph
-    plt.plot(x_axis, test_acc, label="test accuracy")
-    plt.plot(x_axis, val_acc_list, label="validation accuracy")
-    plt.plot(x_axis, acc_list, label="train accuracy")
-    plt.xlabel('epochs')
-    plt.ylabel('value')
-    plt.title('train and validation accuracy')
-    plt.legend()
-    plt.show()
+    results = model.evaluate(X_test, y_test, verbose=0)
+    print("final accuracy: {:5.2f}%".format(100 * results[1]))
--- canny_generator.py
+++ canny_generator.py
@@ -1,3 +1,5 @@
+import math
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -38,6 +40,14 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
         print("An exception occurred: can't convert the image from RGB to YIQ")
         exit(1)
 
+def white_balance(img):
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+    return result
 
 n = 1512
 for i in range(n):
@@ -52,12 +62,56 @@ for i in range(n):
     # yiq[:, :, 0] += edges * 0.1
     # img = transformYIQ2RGB(yiq)
 
-    img = cv2.imread(img_path)
-    img = cv2.bilateralFilter(img, 50, 25, 50)
+    # # bilateral
+    # img = cv2.imread(img_path)
+    # img = cv2.bilateralFilter(img, 50, 25, 50)
+    # # LoG
+    # kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    # sigma = 0.1
+    # img = (1-sigma)*img + sigma*cv2.filter2D(src=img, ddepth=-1, kernel=kernel)
 
-    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    sigma = 0.1
-    img = (1-sigma)*img + sigma*cv2.filter2D(src=img, ddepth=-1, kernel=kernel)
+    # # bilateral + canny
+    # img = cv2.imread(img_path)
+    # img = cv2.bilateralFilter(img, 50, 25, 50)
+    # edges = np.array(cv2.Canny((img * 255).astype(np.uint8), 200, 225) / 255)
+    # edges = np.dstack((edges, edges, edges))
+    # img = img + edges * img
+    # cv2.imwrite(f'database/image_bilateral_224X224/{i}.png', img)
+
+    # # histogram eq
+    # img = cv2.imread(img_path)
+    # img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    # # Histogram equalisation on the V-channel
+    # img_hsv[:, :, 2] = cv2.equalizeHist(img_hsv[:, :, 2])
+    # # convert image back from HSV to RGB
+    # img = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2RGB)
+    # cv2.imwrite(f'database/image_bilateral_224X224/{i}.png', img)
+
+    img = np.array(cv2.imread(img_path))
+    (b, g, r) = cv2.split(img)
+    r_new = r * 0.393 + g * 0.769 + b * 0.189
+    g_new = r * 0.349 + g * 0.686 + b * 0.168
+    b_new = r * 0.272 + g * 0.534 + b * 0.131
+    img2 = cv2.merge([b_new, g_new, r_new])
+    img = np.hstack([
+        img,
+        img2])
+    # img = cv2.bilateralFilter(img, 25, 50, 50)
+    # img = white_balance(img)
+
+    # convert img to HSV
+    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # hue, sat, val = cv2.split(hsv)
+    # # compute gamma = log(mid*255)/log(mean)
+    # mid = 0.5
+    # mean = np.mean(val)
+    # gamma = math.log(mid * 255) / math.log(mean)
+    # print(gamma)
+    # # do gamma correction on value channel
+    # val_gamma = np.power(val, gamma).clip(0, 255).astype(np.uint8)
+    # # combine new value channel with original hue and sat channels
+    # hsv_gamma = cv2.merge([hue, sat, val_gamma])
+    # img = cv2.cvtColor(hsv_gamma, cv2.COLOR_HSV2BGR)
 
     cv2.imwrite(f'database/image_bilateral_224X224/{i}.png', img)
 
@@ -66,3 +120,6 @@ for i in range(n):
     # cv2.imwrite(f'database/image_edge_224X224/{i}.png', (img * 255).astype(np.uint8))
     # plt.imshow(img, cmap='gray')
     # plt.show()
+
+
+
